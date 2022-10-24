04e9442976f5ddf4221822863a959769b4d2d1c5
Veikka <73283358+vhjaho@users.noreply.github.com>
2022-10-24 18:49:59 +0300

    Create response.ts

---
+++ response.ts
@@ -0,0 +1,24 @@
+export interface GetCalendarResults {
+    results: Calendar[];
+}
+
+export interface Calendar {
+  "kind": "calendar#events",
+  "etag": etag,
+  "summary": string,
+  "description": string,
+  "updated": datetime,
+  "timeZone": string,
+  "accessRole": string,
+  "defaultReminders": [
+    {
+      "method": string,
+      "minutes": integer
+    }
+  ],
+  "nextPageToken": string,
+  "nextSyncToken": string,
+  "items": [
+    events Resource
+  ]
+}
